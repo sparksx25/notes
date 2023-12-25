af06139b9e458f760cf792b2aad630c2acfcbd99
zengweifu <zengweifu@jjsnt.com>
2023-12-25 18:35:46 +0800

    algorithm

--- algorithm/addTwoNumbers.ts
+++ algorithm/addTwoNumbers.ts
@@ -10,6 +10,36 @@
  * }
  */
 
-function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+// https://leetcode.cn/problems/add-two-numbers/
 
+
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  let plusses = 0;
+  let node: ListNode | null = null;
+  while(l1 || l2) {
+    const n1 = l1 ? l1.val : 0;
+    const n2 = l2 ? l2.val : 0;
+    const sum = n1 + n2 + plusses;
+    const nextNode = new ListNode(sum % 10)
+    plusses = Math.floor(sum / 10);
+    if (node) {
+      node.next = nextNode;
+    } else {
+      node = nextNode;
+    }
+
+
+    l1 = l1 && l1.next;
+    l2 = l2 && l2.next;
+  }
+  return node;
 };
+
--- algorithm/index.md
+++ algorithm/index.md
@@ -3,3 +3,8 @@
 ## 数组
 - [两数之和](./toSum.ts)
 - [寻找两个正序数组的中位数](./findMedianSortedArrays.ts)
+- [盛最多水的容器](./findMedianSortedArrays.ts)
+
+
+## 不太清楚
+- [盛最多水的容器](./findMedianSortedArrays.ts)
